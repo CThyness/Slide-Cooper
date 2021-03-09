1179cd5b6ba10aaede3b8b9386044fb3acafb2dd
Tage Grønli <tgo@shifthyperloop.com>
2021-03-09 15:54:36 +0100

    Update kode_stepper_control.ino

--- kode_stepper_control/kode_stepper_control.ino
+++ kode_stepper_control/kode_stepper_control.ino
@@ -11,7 +11,7 @@ int sig_2 = 3; //green
 int sig_3 = 4; //blue 
 int sig_4 = 5; //red
 
-  void Spole_1_stom(void){
+void Spole_1_stom(void){
   digitalWrite(sig_1, HIGH);
   digitalWrite(sig_2, LOW);
 }
@@ -38,17 +38,17 @@ void All_GND(void){
 void loop() {
   All_GND();
   Spole_1_stom();
-  delay(10);
+  delay(1000);
 
   All_GND();
   Spole_2_stom();
-  delay(10);
+  delay(1000);
   
   All_GND();
   Spole_1_inv_stom();
-  delay(10);
+  delay(1000);
 
   All_GND();
   Spole_2_inv_stom();
-  delay(10);
+  delay(1000);
 }
